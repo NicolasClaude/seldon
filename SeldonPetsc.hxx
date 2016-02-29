ada583197a7f654f8d27f362a29ce88cde4b7655
Nicolas Claude <Nicolas.Claude@inria.fr>
2016-02-29 15:14:25 +0100

    Added 'MPIDISP', 'MPIERR' and 'IFMAINPROC'.

--- SeldonPetsc.hxx
+++ SeldonPetsc.hxx
@@ -2,6 +2,17 @@
 #define SELDON_FILE_SELDONPETSC_H
 
 
+
+#define IFMAINPROC                                                      \
+  int rank_macro;                                                       \
+  MPI_Comm_rank(PETSC_COMM_WORLD, &rank_macro); if (rank_macro == 0) {
+
+#define MPIDISP(x)  MPIIF( DISP(x) )
+
+#define MPIERR(x)  MPIIF( ERR(x) )
+
+#define MPIIF(x) IFMAINPROC x ;}
+
 #include "vector/PetscVector.cxx"
 #include "matrix/PetscMatrix.cxx"
 
@@ -9,7 +20,5 @@
 #include "computation/basic_functions/Functions_MatrixPetsc.cxx"
 #include "computation/basic_functions/Functions_MatVectPetsc.cxx"
 #include "computation/basic_functions/Functions_VectorPetsc.cxx"
-
-
 
 #endif
